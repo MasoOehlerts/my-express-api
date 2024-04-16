562ef46959c03426b770b8ee97a85a0a4b28069e
MasoOehlerts <Maso.Oehlerts@gmail.com>
2024-04-16 09:16:49 -0400

    homies

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 function MyButton() {
   function handleClick() {
-    alert("I said don't click me you HOE!");
+    alert("I said don't click me asshole!");
   }
   
   return (
@@ -16,25 +16,8 @@ function MyButton() {
 export default function MyApp() {
   return (
     <div>
-      <h1>Welcome to my app Bitches</h1>
+      <h1>Welcome to my app homies</h1>
       <MyButton />
     </div>
   );
 }
-
-
-
-//function MyOtherButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-//}
-//this above is for other button w/ count ^
